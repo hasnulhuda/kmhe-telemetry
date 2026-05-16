dc0f4e12c355d40683874e3406689538da717ee3
hasnulhuda <hasnulhuda118gmailcom@webmail.umm.ac.id>
2026-05-16 13:13:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const client = mqtt.connect(
+'wss://broker.hivemq.com:8884/mqtt'
+);
+
+client.on('connect', function () {
+
+    console.log("MQTT Connected");
+
+    client.subscribe('kmhe/gps');
+
+});
+
+client.on('message', function (topic, message) {
+
+    const data = JSON.parse(message.toString());
+
+    document.getElementById("speed").innerHTML =
+      "Speed : " + data.speed + " km/h";
+
+    document.getElementById("sat").innerHTML =
+      "Satellite : " + data.sat;
+
+    document.getElementById("lat").innerHTML =
+      "Latitude : " + data.lat;
+
+    document.getElementById("lng").innerHTML =
+      "Longitude : " + data.lng;
+
+});
